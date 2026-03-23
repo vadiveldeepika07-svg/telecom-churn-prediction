4f5714ae37937015575d10e5a5580cec54de032b
vadiveldeepika07-svg <vadiveldeepika07@gmail.com>
2026-03-23 22:24:08 +0530

    Create churn_model.py

---
+++ churn_model.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv('churn.csv')
+
+# Preprocessing
+df = df.dropna()
+
+# Convert categorical to numeric
+df = pd.get_dummies(df, drop_first=True)
+
+# Split data
+X = df.drop('Churn', axis=1)
+y = df['Churn']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
